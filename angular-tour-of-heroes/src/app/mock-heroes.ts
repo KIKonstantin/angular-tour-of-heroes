039311c6d44eab4cac7e622c700e6caf24ea9c80
KIKonstantin <kikonstantin@outlook.com>
2022-11-16 20:57:17 +0200

    added hero editor and list display

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 12, name: 'Dr. Nice'},
+    { id: 13, name: 'Bombastic'},
+    { id: 14, name: 'Wolverine'},
+    { id: 15, name: 'Spiderman'},
+    { id: 16, name: 'RubberMan'},
+    { id: 17, name: 'HULK'},
+    { id: 18, name: 'Dr. IQ'},
+    { id: 19, name: 'Magma'},
+    { id: 20, name: 'Torpedo'},
+];
